40c5731263858f3c93cdc452148c463511609c27
RiadSab <sabti.ensa@uhp.ac.ma>
2025-12-01 22:53:49 +0100

    feat(contract): switched from hardhat and ganache to foundry, removed contract address deployment logic from creating a new JSON file, to using the default JSON file created by foundry which contains the contract address

---
+++ evote-foundry/script/Deploy.s.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+import "forge-std/Script.sol";
+import "../src/CommitmentRegistry.sol";
+
+contract DeployScript is Script {
+    function run() external {
+        // 1. Start Broadcasting
+        vm.startBroadcast();
+
+        // 2. Deploy
+        CommitmentRegistry registry = new CommitmentRegistry();
+
+        // 3. Stop Broadcasting
+        vm.stopBroadcast();
+
+        // 4. Log for Humans (Optional)
+        console.log("Deployed CommitmentRegistry at:", address(registry));
+        
+       }
+}
---
+++ evote-foundry/src/CommitmentRegistry.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+contract CommitmentRegistry {
+    // 1. Storage: Maps a voter's address to their secret vote hash
+    mapping(address => bytes32) public commitments;
+
+    // 2. Event: Alerts the world (and Ganache) when a vote happens
+    event CommitmentSubmitted(address indexed voter, bytes32 commitment);
+
+    // 3. Logic: Allow a user to vote, but only once
+    function submitCommitment(bytes32 _commitment) external {
+        require(commitments[msg.sender] == bytes32(0), "Vote already committed");
+
+        commitments[msg.sender] = _commitment;
+
+        emit CommitmentSubmitted(msg.sender, _commitment);
+    }
+}
