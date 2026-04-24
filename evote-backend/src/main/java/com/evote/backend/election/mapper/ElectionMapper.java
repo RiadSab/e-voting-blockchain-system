7cdc255e83191a46c2c2c38a7c911ae23be81681
RiadSab <sabti.ensa@uhp.ac.ma>
2026-04-24 10:43:26 +0100

    feat(backend): move mapping logic from ElectionService to ElectionMapper
    Refactored mapping responsibilities out of ElectionService into a dedicated ElectionMapper.
    
    Handled mappings:
    - ElectionCreateRequest → Election
    - Election → ElectionSummaryDto
    - Election → ElectionDetailsDto
    
    Chose manual mapping to maintain full control over fields and businessº-specific defaults.

--- evote-backend/src/main/java/com/evote/backend/election/mapper/ElectionMapper.java
+++ evote-backend/src/main/java/com/evote/backend/election/mapper/ElectionMapper.java
@@ -1,11 +1,15 @@
 package com.evote.backend.election.mapper;
 
 import com.evote.backend.election.dto.ElectionCreateRequest;
+import com.evote.backend.election.dto.ElectionDetailsDto;
+import com.evote.backend.election.dto.ElectionSummaryDto;
 import com.evote.backend.shared.enums.ElectionStatus;
 import com.evote.backend.election.entity.Election;
+import org.springframework.stereotype.Component;
 
 import java.math.BigInteger;
 
+@Component
 public class ElectionMapper {
     public static Election toElectionEntity(ElectionCreateRequest request, Long semaphoreGroupId, String contractAddress, BigInteger electionIdOnChain) {
         Election entity = new Election();
@@ -21,5 +25,35 @@ public class ElectionMapper {
         entity.setSemaphoreGroupId(semaphoreGroupId);
         entity.setElectionIdOnChain(electionIdOnChain.toString());
         return entity;
+    }
+
+    public static ElectionSummaryDto toElectionSummaryDto(Election election) {
+        return new ElectionSummaryDto(
+                election.getId().toString(),
+                election.getElectionName(),
+                election.getContractAddress(),
+                election.getMetadataCid(),
+                election.getStartTime(),
+                election.getEndTime(),
+                election.getStatus(),
+                election.isClosed(),
+                election.isTallyPublished()
+        );
+    }
+
+    public static ElectionDetailsDto toElectionDetailsDto(Election election) {
+        return new ElectionDetailsDto(
+                election.getId().toString(),
+                election.getElectionName(),
+                election.getContractAddress(),
+                election.getMetadataCid(),
+                election.getMetadataHash(),
+                election.getStartTime(),
+                election.getEndTime(),
+                election.getStatus(),
+                election.getSemaphoreGroupId() != null ? election.getSemaphoreGroupId().toString() : null,
+                election.isTallyPublished(),
+                election.getCandidates()
+        );
     }
 }
