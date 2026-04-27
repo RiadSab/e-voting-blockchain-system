39ea94abacad7a59ccfc219bee786c815d77d54d
RiadSab <sabti.ensa@uhp.ac.ma>
2026-04-27 17:12:13 +0100

    refactor(contracts: ElectionFactory): Adapt the deployment inputs of new elections to match Election constructor

--- evote-foundry/src/ElectionFactory.sol
+++ evote-foundry/src/ElectionFactory.sol
@@ -1,15 +1,13 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
-
-import { Ownable } from "@openzeppelin/contracts/access/Ownable.sol";
-import { Address } from "@openzeppelin/contracts/utils/Address.sol";
-import { ISemaphore } from "@semaphore/contracts/interfaces/ISemaphore.sol";
-import { Election } from "./Election.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+import {Address} from "@openzeppelin/contracts/utils/Address.sol";
+import {ISemaphore} from "@semaphore/contracts/interfaces/ISemaphore.sol";
+import {Election} from "./Election.sol";
 
 contract ElectionFactory is Ownable {
     using Address for address;
-
 
     event ElectionCreated(
         uint256 indexed electionId,
@@ -20,7 +18,6 @@ contract ElectionFactory is Ownable {
         uint256 endTime
     );
 
-
     // starts at 1 for clarity
     uint256 public electionCount;
 
@@ -29,47 +26,45 @@ contract ElectionFactory is Ownable {
     address public electoralAuthority;
     address public semaphore;
 
-
     modifier onlyAuthority() {
         _onlyAuthority();
         _;
     }
 
     function _onlyAuthority() internal view {
-        require(msg.sender == electoralAuthority, "Factory: caller not electoralAuthority");
+        require(
+            msg.sender == electoralAuthority,
+            "Factory: caller not electoralAuthority"
+        );
     }
 
-
-
-    constructor(address _electoralAuthority, address _semaphore) Ownable(_electoralAuthority) {
+    constructor(
+        address _electoralAuthority,
+        address _semaphore
+    ) Ownable(_electoralAuthority) {
         electoralAuthority = _electoralAuthority;
         semaphore = _semaphore;
         electionCount = 1;
     }
-
-
 
     function setElectoralAuthority(address _authority) external onlyOwner {
         require(_authority != address(0), "Factory: authority zero address");
         electoralAuthority = _authority;
     }
 
-
-
     function createElection(
         string calldata ipfsCid, // for retrieval
         bytes32 metadataHash, // for content verification
         uint256 pkx,
         uint256 pky,
+        uint256 registrationStartTime,
+        uint256 registrationEndTime,
         uint256 startTime,
         uint256 endTime,
+        uint256 candidatesCount,
         address tallyVerifier,
         address electionAuthority
-    )
-        external
-        onlyAuthority
-        returns (uint256 newElectionId)
-    {
+    ) external onlyAuthority returns (uint256 newElectionId) {
         require(bytes(ipfsCid).length > 0, "Factory: ipfsCid required");
         require(startTime < endTime, "Factory: invalid time window");
         require(endTime > block.timestamp, "Factory: endTime in past");
@@ -84,8 +79,11 @@ contract ElectionFactory is Ownable {
             metadataHash,
             pkx,
             pky,
+            registrationStartTime,
+            registrationEndTime,
             startTime,
             endTime,
+            candidatesCount,
             tallyVerifier,
             electionAuthority,
             semaphore,
@@ -94,13 +92,22 @@ contract ElectionFactory is Ownable {
 
         elections[newElectionId] = address(e);
 
-        emit ElectionCreated(newElectionId, address(e), metadataHash, ipfsCid, startTime, endTime);
+        emit ElectionCreated(
+            newElectionId,
+            address(e),
+            metadataHash,
+            ipfsCid,
+            startTime,
+            endTime
+        );
         // we can get the electionId from the receipt of the election creation transaction
         //  ex : const event = receipt.logs[0];
         return newElectionId; // can be used by another contract
     }
 
-    function getElectionAddress(uint256 electionId) external view returns (address) {
+    function getElectionAddress(
+        uint256 electionId
+    ) external view returns (address) {
         return elections[electionId];
     }
 }
